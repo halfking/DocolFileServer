7e919b1b47c43df7eeacbbfd6c08fbbd294cf29f
XUTAO HUANG <xutaohuang@XUTAOdeMacBook-Pro.local>
2021-02-04 15:28:03 +0800

    init

---
+++ DocolFileServer/DocolFileServer.h
@@ -0,0 +1,18 @@
+//
+//  DocolFileServer.h
+//  DocolFileServer
+//
+//  Created by XUTAO HUANG on 2021/2/4.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DocolFileServer.
+FOUNDATION_EXPORT double DocolFileServerVersionNumber;
+
+//! Project version string for DocolFileServer.
+FOUNDATION_EXPORT const unsigned char DocolFileServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DocolFileServer/PublicHeader.h>
+
+
